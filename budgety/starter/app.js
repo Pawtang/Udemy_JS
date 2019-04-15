b156e45b8327e2a2b1585fb4f091934aeea59ebb
Pawtang <ben.dibuz@gmail.com>
2019-04-15 01:41:52 +0000

    ...

--- budgety/starter/app.js
+++ budgety/starter/app.js
@@ -82,7 +82,11 @@ var UIController = (function(){
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
     return{
         getInput: function() {
@@ -118,7 +122,20 @@ var UIController = (function(){
                 });
                 fieldsArr[0].focus();
             },
-        getDOMstrings: function() {
+
+          displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            if (obj.percentage > 0) {
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+          } else {
+            document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+          }
+          },
+
+          getDOMstrings: function() {
             return DOMstrings;
             }
           };
@@ -141,7 +158,7 @@ var controller = (function(budgetCtrl, UICtrl){
     var updateBudget = function() {
       budgetCtrl.calculateBudget();
       var budget = budgetCtrl.getBudget();
-      console.log(budget);
+      UIController.displayBudget(budget);
     };
 
     var ctrlAddItem = function(){
@@ -159,6 +176,12 @@ var controller = (function(budgetCtrl, UICtrl){
         init: function(){
           console.log('App has started');
           setupEventListeners();
+          UIController.displayBudget({
+            budget: 0,
+            totalInc: 0,
+            totalExp: 0,
+            percentage: -1
+          })
         }
     };
 })(budgetController, UIController);
